e81d227d06ef0e4626e35f80cb87eafcf15ef567
uellenberg <JonahUellenberg@hotmail.com>
2021-11-03 14:58:22 -0700

    Actually fix compiler errors

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@ import {TemplateState} from "./types/TemplateState";
 import {SemiMutable} from "./SemiMutable";
 
 export const ensureState = (state: TemplateState) => {
-    if (!state.hasOwnProperty("graphgame")) state.graphgame = { objects: {}, actions: [], finalActions: [], finalized: false };
+    if (!state.hasOwnProperty("graphgame")) state.graphgame = { objects: {}, behaviors: {}, actions: [], finalActions: [], finalized: false };
     if(state.graphgame.finalized) throw new Error("Do not run any other templates after finalizing!");
 };
 
