1ce54550e89303b6f816b04732391b5199cd79a8
uellenberg <JonahUellenberg@hotmail.com>
2021-12-04 23:01:47 -0800

    Update circle to newest version

--- src/init/circle.ts
+++ src/init/circle.ts
@@ -2,10 +2,10 @@ export default `
 createBehavior!("circle");
 
 behaviorGraph!("circle", {
-    const p1 = x-getBehaviorVal!("circle", "base.transform.x");
-    const p2 = y-getBehaviorVal!("circle", "base.transform.y");
+    const p1 = x-getVal!("circle", "base.transform.x");
+    const p2 = y-getVal!("circle", "base.transform.y");
 
-    state = (p1^2+p2^2)/getBehaviorVal!("circle", "base.transform.scale");
+    state = (p1^2+p2^2)/getVal!("circle", "base.transform.scale");
 });
 
 finalizeBehavior!("circle");
