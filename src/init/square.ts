e72e848b5760ab2031318f74cfc38cbf6b4ecf37
uellenberg <JonahUellenberg@hotmail.com>
2021-12-05 00:24:27 -0800

    Fix square's scale

--- src/init/square.ts
+++ src/init/square.ts
@@ -2,11 +2,11 @@ export default `
 createBehavior!("square");
 
 behaviorGraph!("square", {
+    const rot = pi/4;
     const xpos = getVal!("square", "base.transform.x");
     const ypos = getVal!("square", "base.transform.y");
-    const xscale = getVal!("square", "base.transform.scale_x");
-    const yscale = getVal!("square", "base.transform.scale_y");
-    const rot = pi/4;
+    const xscale = getVal!("square", "base.transform.scale_x")*rot;
+    const yscale = getVal!("square", "base.transform.scale_y")*rot;
 
     const p1 = ((x-xpos)/xscale)*cos(rot) + ((y-ypos)/yscale)*sin(rot);
     const p2 = ((y-ypos)/yscale)*cos(rot) - ((x-xpos)/xscale)*sin(rot);
