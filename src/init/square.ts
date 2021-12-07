5338d294c2e57853b83d99bae0739b4d287cbb39
uellenberg <JonahUellenberg@hotmail.com>
2021-12-07 00:06:05 -0800

    Make square renderer nicer

--- src/init/square.ts
+++ src/init/square.ts
@@ -2,7 +2,14 @@ export default `
 createBehavior!("square");
 
 behaviorCustom!("square", {
-    polygon((getVal!("square", "base.transform.x"), getVal!("square", "base.transform.y")) + getVal!("square", "base.transform.scale_x")*[(-1/2, 0), (1/2, 0), (1/2, 0), (-1/2, 0)] + getVal!("square", "base.transform.scale_y")*[(0, -1/2), (0, -1/2), (0, 1/2), (0, 1/2)]);
+    polygon({
+        const x = getVal!("square", "base.transform.x");
+        const y = getVal!("square", "base.transform.y");
+        const x_scale = getVal!("square", "base.transform.scale_x");
+        const y_scale = getVal!("square", "base.transform.scale_y");
+    
+        state = (x, y) + x_scale*[(-1/2, 0), (1/2, 0), (1/2, 0), (-1/2, 0)] + y_scale*[(0, -1/2), (0, -1/2), (0, 1/2), (0, 1/2)];
+    });
 }, 300);
 
 finalizeBehavior!("square");
