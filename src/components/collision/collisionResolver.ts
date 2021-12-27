1d0eb1270a489c268bad865f5fd34fbd8c32fb08
uellenberg <JonahUellenberg@hotmail.com>
2021-12-26 19:36:44 -0800

    Make the collision resolver favor one axis

--- src/components/collision/collisionResolver.ts
+++ src/components/collision/collisionResolver.ts
@@ -1,24 +1,25 @@
 export default `
 createBehavior!("collision_resolver");
 
-helper!("collision_resolver", {
-    export function g_raphgame_collision_resolver_helper(a, b) {
-        state = 0;
-        if(abs(a) <= abs(b)) {
-            state = a;
-        }
-    }
-});
-
 setMut!("collision_resolver", "base.transform.x");
 setMut!("collision_resolver", "base.transform.y");
 
+//The reason that x is <= and y is < is that, if both are <=, then we can possibly put the object at a corner. This makes sure that it always goes on an edge.
+
 setValAction!("collision_resolver", "base.transform.x", {
-    state = x + g_raphgame_collision_resolver_helper(getVal!("collision_resolver", "base.transform.collision_x"), getVal!("collision_resolver", "base.transform.collision_y"));
+    if(abs(getVal!("collision_resolver", "base.transform.collision_x")) <= abs(getVal!("collision_resolver", "base.transform.collision_y"))) {
+        state = x + getVal!("collision_resolver", "base.transform.collision_x");
+    } else {
+        state = x;
+    }
 }, -200);
 
 setValAction!("collision_resolver", "base.transform.y", {
-    state = y + g_raphgame_collision_resolver_helper(getVal!("collision_resolver", "base.transform.collision_y"), getVal!("collision_resolver", "base.transform.collision_x"));
+    if(abs(getVal!("collision_resolver", "base.transform.collision_y")) < abs(getVal!("collision_resolver", "base.transform.collision_x"))) {
+        state = y + getVal!("collision_resolver", "base.transform.collision_y");
+    } else {
+        state = y;
+    }
 }, -200);
 
 finalizeBehavior!("collision_resolver");
