97fe50a5f653a401f21e0168e9e5bb0f34af7ea1
uellenberg <JonahUellenberg@hotmail.com>
2021-12-26 19:20:52 -0800

    Make doa values inline

--- src/components/doa/doa.ts
+++ src/components/doa/doa.ts
@@ -43,16 +43,16 @@ helper!("doa", {
 setValArgs!("doa", "layer", 0);
 
 setVal!("doa", "base.transform.x", 0);
-setMut!("doa", "base.transform.x", 0);
+setInline!("doa", "base.transform.x", 0);
 
 setVal!("doa", "base.transform.y", 0);
-setMut!("doa", "base.transform.y", 0);
+setInline!("doa", "base.transform.y", 0);
 
 setVal!("doa", "base.transform.scale_x", 0);
-setMut!("doa", "base.transform.scale_x", 0);
+setInline!("doa", "base.transform.scale_x", 0);
 
 setVal!("doa", "base.transform.scale_y", 0);
-setMut!("doa", "base.transform.scale_y", 0);
+setInline!("doa", "base.transform.scale_y", 0);
 
 setVal!("doa", "idx", 0);
 setInline!("doa", "idx");
@@ -71,7 +71,7 @@ setValAction!("doa", "idx", {
             }
         };
     });
-});
+}, -500);
 
 setValAction!("doa", "base.transform.x", {
     state = 0;
@@ -81,7 +81,7 @@ setValAction!("doa", "base.transform.x", {
     
     //Find the object.
     state = g_raphgame_doa_x_array[id];
-});
+}, -500);
 
 setValAction!("doa", "base.transform.y", {
     state = 0;
@@ -91,7 +91,7 @@ setValAction!("doa", "base.transform.y", {
     
     //Find the object.
     state = g_raphgame_doa_y_array[id];
-});
+}, -500);
 
 setValAction!("doa", "base.transform.scale_x", {
     state = 0;
@@ -101,7 +101,7 @@ setValAction!("doa", "base.transform.scale_x", {
     
     //Find the object.
     state = g_raphgame_doa_xs_array[id];
-});
+}, -500);
 
 setValAction!("doa", "base.transform.scale_y", {
     state = 0;
@@ -111,7 +111,7 @@ setValAction!("doa", "base.transform.scale_y", {
     
     //Find the object.
     state = g_raphgame_doa_ys_array[id];
-});
+}, -500);
 
 finalizeBehavior!("doa");
 
