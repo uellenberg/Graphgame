34bfd2d45e600a0122e068ee89185ab36108017b
uellenberg <JonahUellenberg@hotmail.com>
2021-12-26 19:34:00 -0800

    Remove active doa

--- src/components/doa/window.ts
+++ src/components/doa/window.ts
@@ -9,24 +9,6 @@ setValArgs!("window", "base.transform.scale_y", 1, 1);
 setVal!("window", "base.transform.x", 0);
 setVal!("window", "base.transform.y", 0);
 
-setVal!("window", "active_doa", -1);
-setMut!("window", "active_doa");
-setValAction!("window", "active_doa", {
-    //Move to the next doa.
-    state = active_doa + 1;
-    
-    const max_object = {
-        state = -1;
-    
-        selectBehavior!("doa", {
-            state = selectedID!();
-        });
-    };
-    
-    //Limit it to the highest object ID.
-    state = mod(state, max_object+1);
-});
-
 finalizeBehavior!("window");
 
 //Prefab
