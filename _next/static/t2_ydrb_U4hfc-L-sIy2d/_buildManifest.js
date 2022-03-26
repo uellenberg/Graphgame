cad8e4862d9b325cf6cd733747dfe430e36d44de
uellenberg <JonahUellenberg@hotmail.com>
2022-03-26 22:37:56 +0000

    Rebuild Github Pages

--- _next/static/t2_ydrb_U4hfc-L-sIy2d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-14aa0a1277a0c3d0.js"],"/_error":["static/chunks/pages/_error-a3f18418a2205cb8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
