9a26ba63054b07bbe9af2fc4b8bcddb291335a3e
uellenberg <JonahUellenberg@hotmail.com>
2023-10-03 23:26:12 +0000

    Rebuild Github Pages

--- _next/static/Jp3k_npjmE6OrVVQwXhSg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-19d489ba0f18e656.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/pOaLuzS4tN5jUS3Ka4vh5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-729d7e08e923ba02.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
